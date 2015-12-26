8ab696a4f27a327188c43062a87ff70d98f4ecb3
dm4000 <daniel@listretto.com>
2015-12-26 20:15:42 +0900

    Add "levels" and also iterate over nicknames array

--- js/app.js
+++ js/app.js
@@ -6,25 +6,39 @@ var ViewModel = function () {
   //
   // this is actually putting the Model inside the ViewModel (controller)
   // however, there is still FUNCTIONAL separation of concerns
-  
-  
+
+
   // initialise new property "clickCount" and set its value to "0"
   this.clickCount = ko.observable(0);
-  
+
   // initialise new property "name" and set its value to "Sally"
   this.name = ko.observable('Sally');
   
-  
+  this.nicknames = ko.observableArray([
+    {nickname1: "Sal",
+     nickname2: "S-Lo", 
+     nickname3: "Salzzzz"
+    }
+  ]);
+
+
+  this.level = ko.computed(function () {
+    if (this.clickCount() < 11) {
+      return "Newborn";
+    } else {
+      return "Infant";
+    }
+  }, this);
 
   this.imgSrc = ko.observable('img/Sally.jpg');
   this.imgAttribution = ko.observable('http://ampersandmoi.com');
 
-   
+
   //
   // CONTROLLER = VIEWMODEL
   //
   // liases with data, based on user interaction with view
-  
+
   this.incrementCounter = function () {
     // SET clickCount to clickCount() (which returns current clickCount) PLUS 1
     // remember that returning values in KO requires executing the function
