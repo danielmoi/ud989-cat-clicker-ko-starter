202248e7f507978a01252a17920e655d857a0e16
dm4000 <daniel@listretto.com>
2015-12-29 09:21:35 +0900

    Separate data into own Model (function) and adjust references accordingly

--- js/app.js
+++ js/app.js
@@ -1,22 +1,14 @@
-// create our data object "ViewModel"
-var ViewModel = function () {
+// **
+// Model
+// **
 
-  //
-  // DATA = MODEL
-  //
-  // this is actually putting the Model inside the ViewModel (controller)
-  // however, there is still FUNCTIONAL separation of concerns
-
-
-  // initialise new property "clickCount" and set its value to "0"
+var Cat = function () {
   this.clickCount = ko.observable(0);
 
-  // initialise new property "name" and set its value to "Sally"
   this.name = ko.observable('Sally');
 
   this.nicknames = ko.observableArray(
     ["Sal", "S-Lo", "Salzzzz", "Salaza"]
-    
   );
 
 
@@ -25,8 +17,7 @@ var ViewModel = function () {
     var clicks = this.clickCount();
     if (clicks < 11) {
       title = "Newborn";
-    } 
-    else if (clicks < 20) {
+    } else if (clicks < 20) {
       title = "Infant";
     } else {
       title = "Ninja";
@@ -37,21 +28,24 @@ var ViewModel = function () {
   this.imgSrc = ko.observable('img/Sally.jpg');
 
   this.imgAttribution = ko.observable('http://ampersandmoi.com');
+};
+
+// **
+// ViewMmodel
+// **
+
+var ViewModel = function () {
+  
+  this.currentCat = ko.observable( new Cat() );
+  
 
 
-  //
-  // CONTROLLER = VIEWMODEL
-  //
-  // liases with data, based on user interaction with view
+
 
   this.incrementCounter = function () {
-    // SET clickCount to clickCount() (which returns current clickCount) PLUS 1
-    // remember that returning values in KO requires executing the function
-    // ie. clickCount() and not clickCount (which would return the function)
-    this.clickCount(this.clickCount() + 1)
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1)
   };
 
 };
 
-// Tell KO to apply the bindings to our data object ("ViewModel", which is a function)
 ko.applyBindings(new ViewModel());
